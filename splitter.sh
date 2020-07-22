0c2f2506f43b805be330596cf63e45622d5c2429
Iscle <albertiscle9@gmail.com>
2020-07-22 20:37:39 +0200

    Added identical repo detection and file output

--- splitter.sh
+++ splitter.sh
@@ -1,7 +1,9 @@
-bsp_path='/speedy/t-alps-release-q0.mp1-V9.77'
-manifest="android-10.0.0_r2"
-branch_name="mtk_v9.77_10.0"
-temp_dir_path='/speedy/testing/temp'
+#!/bin/bash
+
+bsp_path='/media/iscle/AndroidSources/alps_q/V9.107/t-alps-q0.mp1-V9.107'
+manifest='android-10.0.0_r2'
+branch_name='t-alps-q0.mp1-V9.107'
+temp_dir_path='/media/iscle/AndroidSources/temp'
 
 # The following repos need to be done manually due to symlinks
 # build/make
@@ -9,28 +11,44 @@ temp_dir_path='/speedy/testing/temp'
 # hardware/qcom/sdm845/data/ipacfg-mgr
 # sdk
 
+# Get the current directory for later
+cur_dir=$(pwd)
+
+# Delete any previous temp folder
+rm -rf $temp_dir_path
+# Delete any previous files
+rm -f not_found.txt
+rm -f no_changes.txt
+
 # Known repo detection
-cat repos.txt | while read -r line;
-do
-if [ ! -d "${bsp_path}/${line}/" ] 
-    then
-	    echo /${line}/ not found
-    else
-	    #echo ${bsp_path}/${line}/
-	    cd ${bsp_path}/${line}/
-		cp -rf * ${temp_dir_path}
+while read -r line; do
+    if [ -d "${bsp_path}/${line}/" ]; then
+        #echo ${bsp_path}/${line}/
+        mkdir $temp_dir_path
+        cd ${temp_dir_path}/
         git init
-		git checkout -b temp
-        git add --all
-        git commit -a -m "BSP Changes"
-		git remote add google https://android.googlesource.com/platform/$line
-		git fetch --all
-		git checkout tags/android-10.0.0_r2 -b $branch_name
-		cp -rf ${temp_dir_path}/* .
-		git add --all
-		git commit -a -m "MTK Changes"
-		hub create mtk-watch/android_$(echo "${line}"|sed 's#/#_#g')
-		git push -f origin $branch_name
-		rm -rf ${temp_dir_path}/*
+        git remote add google https://android.googlesource.com/platform/$line
+        git fetch google
+        git checkout tags/${manifest} -b $branch_name
+        cp -rf ${bsp_path}/${line}/* .
+
+        if [[ -z $(git status -s) ]]; then
+            echo /${line}/ has no changes!
+            echo $line >> ${cur_dir}/no_changes.txt
+        else
+            git add --all
+            git commit -m "Add MediaTek changes
+Branch: ${branch_name}"
+            hub create mtk-watch/android_$(echo "${line}" | sed 's#/#_#g')
+            git push -f origin $branch_name
+        fi
+
+        rm -rf $temp_dir_path
+    else
+        echo Warning: /${line}/ not found!
+        echo $line >> ${cur_dir}/not_found.txt
     fi
-done
+done < repos.txt
+
+# Cleanup
+rm -rf $temp_dir_path
