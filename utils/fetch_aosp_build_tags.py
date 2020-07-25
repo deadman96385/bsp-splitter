ff40f931530ed7b19894c3abffd65c3aea082898
Greg Willard (r3pwn) <wickett06@gmail.com>
2020-07-25 02:58:34 -0400

    [utils] refactor scripts to be importable as python modules

---
+++ utils/fetch_aosp_build_tags.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import json
+import requests
+import os
+from bs4 import BeautifulSoup
+
+output_filename = "build-tags.json"
+
+# the css selector used to locate the "Tags and Builds" table on the page
+builds_table_css_selector = "#source-code-tags-and-builds ~ p > table"
+
+def fetch_latest_build_tags():
+    page = requests.get("https://source.android.com/setup/start/build-numbers")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    try:
+        table = soup.select(builds_table_css_selector)[0]
+    except:
+        print("Unable to locate source code tags table")
+        exit()
+
+    tags_json = []
+
+    # Get table headers
+    print("Extracting table headers...")
+    try:
+        headers = table.find('tr').find_all('th')
+        for i,header in enumerate(headers):
+            headers[i] = header.get_text()
+    except:
+        print("Unable to extract table headers!")
+        exit()
+
+    # Get all non-header table rows
+    print("Extracting table contents...")
+    try:
+        for row in table.find_all('tr')[1:]:
+            build_json = {}
+            for i,cell in enumerate(row.find_all('td')):
+                # populate the value with "N/A" if no data is available
+                build_json[headers[i]] = cell.get_text() if cell.get_text() != '' else 'N/A'
+            tags_json.append(build_json)
+    except:
+        print("Unable to extract table contents!")
+        exit()
+
+    print("Saving to '{}'...".format(output_filename))
+    try:
+        script_dir = os.path.dirname(__file__)
+        output_file = os.path.join(script_dir, "../config/{}".format(output_filename))
+
+        f = open(output_file, 'w')
+        f.write(json.dumps(tags_json, indent=4))
+        f.close()
+    except:
+        print("Unable to save contents to file!")
+        exit()
+
+    print("Done!")
+
+if __name__ == '__main__':
+    fetch_latest_build_tags();
---
+++ utils/fetch_aosp_repo_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import argparse
+import base64
+import json
+import os
+import requests
+from bs4 import BeautifulSoup
+
+script_dir = os.path.dirname(__file__)
+
+output_filename = "project-list.json"
+settings_filename = "settings.json"
+
+try:
+    settings_path = os.path.join(script_dir, "../config/{}".format(settings_filename))
+
+    settings_file = open(settings_path, 'r')
+    settings = json.loads(settings_file.read())
+    settings_file.close()
+except:
+    print("Unable to read {}!".format(settings_filename))
+    exit()
+    
+excluded_projects = settings['settings']['excluded_projects']
+
+parser = argparse.ArgumentParser(description='Fetch a list of project paths for a given AOSP tag')
+parser.add_argument('-t', '--tag', help='AOSP tag to pull the manifest for', default='android-10.0.0_r2')
+args = parser.parse_args()
+
+manifest_base = "https://android.googlesource.com/platform/manifest"
+tag_name = args.tag
+
+def fetch_manifest_for_tag(tag):
+    print("Fetching manifest for tag '{}'".format(tag))
+
+    base64_manifest_xml = requests.get("{}/+/refs/heads/{}/default.xml?format=TEXT".format(manifest_base, tag))
+    if base64_manifest_xml.status_code != 200:
+        print("Invalid tag specified!")
+        exit()
+
+    manifest_xml = base64.b64decode(base64_manifest_xml.text)
+    manifest = BeautifulSoup(manifest_xml, features="html.parser")
+
+    # Find all 'project' elements in the manifest
+    projects = []
+    for project in manifest.find_all('project'):
+        project_path = project.get('path')
+        if project_path not in excluded_projects:
+            projects.append(project_path)
+
+    projects_json = {"tag": tag, "projects": projects}
+
+    print("Saving to '{}'...".format(output_filename))
+    try:
+        output_path = os.path.join(script_dir, "../config/{}".format(output_filename))
+
+        output_file = open(output_path, 'w')
+        output_file.write(json.dumps(projects_json, indent=4))
+        output_file.close()
+    except:
+        print("Unable to save contents to file!")
+        exit()
+
+    print("Done!")
+    
+if __name__ == '__main__':
+    fetch_manifest_for_tag(tag_name);
